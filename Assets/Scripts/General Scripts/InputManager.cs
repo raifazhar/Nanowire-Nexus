849bd3f8d60b0b8fc92c74725bea2e79542c0e84
Monarch789 <muhammadarhamali003@gmail.com>
2024-02-21 19:05:53 +0500

    Added the Input Manager
    Along with a little starting of morse code minigame

---
+++ Assets/Scripts/General Scripts/InputManager.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour{
+
+    //Events for Player UI Minigames
+
+    public event EventHandler OnUpKeyPress;
+    public event EventHandler OnDownKeyPress;
+    public event EventHandler OnLeftKeyPress;
+    public event EventHandler OnRightKeyPress;
+    public event EventHandler OnSubmit;
+    public event EventHandler OnChangeToSpider;
+
+
+    //Events for Spider
+    public event EventHandler OnInteract;
+    public event EventHandler OnChangeToPlayer;
+
+    //Singleton Pattern
+    public static InputManager Instance { get; private set; }
+
+    private PlayerInputActions inputActions;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Start() {
+        inputActions.Spider.Disable();
+        inputActions.Player.Enable();
+
+        inputActions.Player.Up.performed += Up_performed;
+        inputActions.Player.Down.performed += Down_performed;
+        inputActions.Player.Left.performed += Left_performed;
+        inputActions.Player.Right.performed += Right_performed;
+        inputActions.Player.Submit.performed += Submit_performed;
+        inputActions.Player.SwitchToSpider.performed += SwitchToSpider_performed;
+
+
+        inputActions.Spider.SwitchToHuman.performed += SwitchToHuman_performed;
+        inputActions.Spider.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnInteract?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void SwitchToHuman_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnChangeToPlayer?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void SwitchToSpider_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnChangeToSpider?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Submit_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnSubmit?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Right_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnRightKeyPress?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Left_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnLeftKeyPress?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Down_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnDownKeyPress?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Up_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnUpKeyPress?.Invoke(this, EventArgs.Empty);
+    }
+
+    // function for spider to get movement
+    Vector2 GetSpiderMovement() {
+        return inputActions.Spider.Movement.ReadValue<Vector2>();
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/Morsecode Scriptable Objects/MorsecodeAlphabetScriptableObject.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Minigames SOs/Morsecode SOs/Morsecode Alphabet Code SO")]
+public class MorsecodeAlphabetScriptableObject : ScriptableObject{
+    public string[] AlphabetCodes;
+}
