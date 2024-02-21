183b1a40c8585e9d6b65e9bcf085493112ff1654
Monarch789 <muhammadarhamali003@gmail.com>
2024-02-21 21:07:53 +0500

    Updated Morse Code game
    blah blah

--- Assets/Scripts/Minigames Scripts/MorseCode Scripts/RandomMorseCodePrint.cs
+++ Assets/Scripts/Minigames Scripts/MorseCode Scripts/RandomMorseCodePrint.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RandomMorseCodePrint : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class RandomMorseCodePrint : MonoBehaviour{
+
+
+    [SerializeField] private TextMeshProUGUI MorseCodeReference;
+
+    [SerializeField] private MorsecodeAlphabetScriptableObject morsecodeScriptableObject;
+
+    [SerializeField] private InputField inputField;
+
+    private string RandomWord;
+
+    private string RandomWordCode;
+
+    private void Start() {
+        //this is just for testing. Event will be added for it to do random
+        for (int i = 0; i < 5; i++) {
+            int RandWordIndex = UnityEngine.Random.Range(1,26);
+
+            RandomWord += 'A' + RandWordIndex - 1;
+
+            RandomWordCode += morsecodeScriptableObject.AlphabetCodes[RandWordIndex-1];
+            RandomWordCode += "\n";
+        }
+        MorseCodeReference.text = RandomWordCode;
+
+
+        InputManager.Instance.OnSubmit += InputManager_OnSubmit;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void InputManager_OnSubmit(object sender, EventArgs e) {
         
     }
 }
