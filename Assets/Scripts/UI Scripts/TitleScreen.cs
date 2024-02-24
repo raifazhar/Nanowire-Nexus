b344ed70cc69abb61dff5df9cac9cd2aeaa1139b
Monarch789 <muhammadarhamali003@gmail.com>
2024-02-24 17:09:03 +0500

    Title Screen Added

---
+++ Assets/Scripts/UI Scripts/TitleScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    [SerializeField] private Button PlayButton;
+    [SerializeField] private Button QuitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI Scripts/TitleScreenImageColor.cs
@@ -0,0 +1,30 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreenImageColor : MonoBehaviour
+{
+
+    private Graphic image;
+    [SerializeField] private float TransitionSpeed = .2f;
+    private void Start()
+    {
+        image = GetComponent<Graphic>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Color currentColor = image.color;
+
+        float h, s, v;
+        Color.RGBToHSV(currentColor,out h,out s,out v);
+
+        h = (h + TransitionSpeed * Time.deltaTime);
+        Color newColor = Color.HSVToRGB(h,s,v);
+
+        image.color = newColor;
+    }
+}
